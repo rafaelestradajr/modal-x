ebb77a5877c0302eebe5f5f85870adf3b391a248
Rafael Estrada <rafaelestrada@comcast.net>
2021-05-20 21:45:53 -0700

    modal

---
+++ app.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", initializedApp);
+
+function initializedApp() {
+  const modal = new Modal(document.querySelector(".modal"));
+  modal.init();
+}
+class Modal {
+  constructor(modal) {
+    this.modalWrapper = modal.parentElement;
+    this.openButton = modal.parentElement.previousElementSibling;
+    this.closeButton = modal.firstElementChild;
+    this.submitButton = modal.querySelector("#modal-submit");
+  }
+
+  init() {
+    this.doEventBindings();
+  }
+  showModal() {
+    console.log("opening the modal...");
+    this.modalWrapper.style.display = "block";
+  }
+  closeModal() {
+    console.log("closing the modal...");
+    this.modalWrapper.style.display = "none";
+  }
+  doEventBindings() {
+    this.openButton.addEventListener("click", this.showModal.bind(this));
+    this.closeButton.addEventListener("click", this.closeModal.bind(this));
+    this.submitButton.addEventListener("click", this.closeModal.bind(this));
+    window.addEventListener('click',event => {
+      if(event.target === this.modalWrapper){
+         this.closeModal();
+      }else {
+          return false;
+      }
+
+    })
+  }
+}
